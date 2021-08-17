17ae7b04dc8c89d52d2d66c76f8d8bff4f4ac9f2
Geo Grigor <geogrigor@gmail.com>
2021-08-17 20:03:58 +0200

    fix config import

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 import json
 from download_sorter import DownloadSorter
 
-with open("config.json") as json_data_file:
+with open("download_sorter/config.json") as json_data_file:
     config = json.load(json_data_file)
 
 instance = DownloadSorter(config['directory'], config['folder_file_map'],
